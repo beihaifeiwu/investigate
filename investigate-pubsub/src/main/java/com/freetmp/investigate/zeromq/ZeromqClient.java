5303522c21a0e1d3ba4931ac48bfae1a25ced96d
liu pin <pin.liu@palmaplus.com>
2015-03-30 09:36:48 +0800

    add zeromq investigation

--- investigate-pubsub/src/main/java/com/freetmp/investigate/zeromq/ZeromqClient.java
+++ investigate-pubsub/src/main/java/com/freetmp/investigate/zeromq/ZeromqClient.java
@@ -1,7 +1,32 @@
 package com.freetmp.investigate.zeromq;
 
+import org.zeromq.ZMQ;
+
 /**
  * Created by LiuPin on 2015/3/28.
  */
 public class ZeromqClient {
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 100; i++){
+            final int out = i;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    ZMQ.Context context = ZMQ.context(1);
+                    ZMQ.Socket subscriber = context.socket(ZMQ.SUB);
+                    subscriber.connect("tcp://127.0.0.1:5555");
+                    subscriber.subscribe("topic".getBytes());
+
+                    for(int j = 0; j < 100; j++){
+                        byte[] message = subscriber.recv();
+                        System.out.println("receive "+ out +"-" +j+" : " + new String(message));
+                    }
+
+                    subscriber.close();
+                    context.term();
+                }
+            }).start();
+        }
+    }
 }
--- investigate-pubsub/src/main/java/com/freetmp/investigate/zeromq/ZeromqServer.java
+++ investigate-pubsub/src/main/java/com/freetmp/investigate/zeromq/ZeromqServer.java
@@ -1,7 +1,27 @@
 package com.freetmp.investigate.zeromq;
 
+import org.zeromq.ZMQ;
+
 /**
  * Created by LiuPin on 2015/3/28.
  */
 public class ZeromqServer {
+
+    public static void main(String[] args) {
+
+        // 创建一个包含一个I/O线程的context
+        ZMQ.Context context = ZMQ.context(1);
+        // 创建一个publisher类型的socket
+        ZMQ.Socket publisher = context.socket(ZMQ.PUB);
+        // 将当前publisher绑定到5555端口上，可以接受subscriber的订阅
+        publisher.bind("tcp://*:5555");
+
+        while (!Thread.currentThread().isInterrupted()){
+            String message = "topic hello";
+            publisher.send(message);
+        }
+
+        publisher.close();
+        context.term();
+    }
 }
