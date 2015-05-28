9a36fe3a55b7a87878752620f7d733801270a164
liu pin <pin.liu@palmaplus.com>
2015-05-28 11:51:13 +0800

    make complete for last module

---
+++ investigate-apidoc/src/main/java/com/freetmp/investigate/apidoc/domain/link/package-info.java
@@ -0,0 +1,6 @@
+@XmlSchema (
+  namespace = "http://api.example.com/link"
+)
+package com.freetmp.investigate.apidoc.domain.link;
+
+import javax.xml.bind.annotation.XmlSchema;
---
+++ investigate-apidoc/src/main/java/com/freetmp/investigate/apidoc/domain/persona/package-info.java
@@ -0,0 +1,6 @@
+@XmlSchema (
+  namespace = "http://api.example.com/persona"
+)
+package com.freetmp.investigate.apidoc.domain.persona;
+
+import javax.xml.bind.annotation.XmlSchema;
