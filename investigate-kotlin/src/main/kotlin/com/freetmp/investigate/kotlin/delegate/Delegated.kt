7061413f17808c1b7bd99a09cff3e08e2ea07292
liu pin <pin.liu@palmaplus.com>
2015-06-04 18:15:27 +0800

    full fill kotlin with grammar example

---
+++ investigate-kotlin/src/main/kotlin/com/freetmp/investigate/kotlin/delegate/Delegated.kt
@@ -0,0 +1,105 @@
+package com.freetmp.investigate.kotlin.delegate
+
+import kotlin.properties.Delegates
+
+/**
+ * Created by LiuPin on 2015/6/4.
+ */
+
+fun main(args: Array<String>) {
+  println("*****************Custom delegate*****************")
+  val e = Example()
+  println(e.p)
+  e.p = "NEW"
+
+  println("****************Lazy property*****************")
+  var sample = LazyExample()
+  println("lazy = ${sample.lazy}")
+  println("lazy = ${sample.lazy}")
+
+  println("****************Observable property****************")
+  var user = User()
+  user.name = "Carl"
+
+  println("****************NotNull property******************")
+  var human = Human()
+  human.init("Carl")
+  println(user.name)
+
+  println("*****************Properties in map*****************")
+  val female = Female(mapOf("name" to "John Doe", "age" to 25))
+  println("name = ${female.name}, age = ${female.age}")
+
+}
+
+/**
+ * There's some new syntax: you can say `val 'property name': 'Type' by 'expression'`.
+ * The expression after by is the delegate, because get() and set() methods
+ * corresponding to the property will be delegated to it.
+ * Property delegates don't have to implement any interface, but they have
+ * to provide methods named get() and set() to be called.</p>
+ */
+class Example {
+  var p: String by Delegate()
+  override fun toString() = "Example Class"
+}
+
+class Delegate() {
+  fun get(thisRef: Any?, prop: PropertyMetadata): String {
+    return "$thisRef, thank you for delegating '${prop.name}' to me!"
+  }
+
+  fun set(thisRef: Any?, prop: PropertyMetadata, value: String) {
+    println("$value has been assigned to ${prop.name} in $thisRef")
+  }
+}
+
+/**
+ * Delegates.lazy() is a function that returns a delegate that implements a lazy property:
+ * the first call to get() executes the lambda expression passed to lazy() as an argument
+ * and remembers the result, subsequent calls to get() simply return the remembered result.
+ * If you want thread safety, use blockingLazy() instead: it guarantees that the values will
+ * be computed only in one thread, and that all threads will see the same value.
+ */
+class LazyExample {
+  val lazy: String by Delegates.lazy { println("computed");"my lazy" }
+}
+
+/**
+ * The observable() function takes two arguments: initial value and a handler for modifications.
+ * The handler gets called every time we assign to `name`, it has three parameters:
+ * a property being assigned to, the old value and the new one. If you want to be able to veto
+ * the assignment, use vetoable() instead of observable().
+ */
+class User {
+  var name: String by Delegates.observable("no name") {
+    d, old, new ->
+    println("$old - $new")
+  }
+}
+
+/**
+ * Users frequently ask what to do when you have a non-null var, but you don't have an
+ * appropriate value to assign to it in constructor (i.e. it must be assigned later)?
+ * You can't have an uninitialized non-abstract property in Kotlin. You could initialize it
+ * with null, bit then you'd have to check every time you access it. Now you have a delegate
+ * to handle this. If you read from this property before writing to it, it throws an exception,
+ * after the first assignment it works as expected.
+ */
+class Human {
+  var name: String by Delegates.notNull()
+  fun init(name: String) {
+    this.name = name
+  }
+}
+
+/**
+ * Properties stored in a map. This comes up a lot in applications like parsing JSON
+ * or doing other "dynamic" stuff. Delegates take values from this map (by the string keys -
+ * names of properties). Of course, you can have var's as well (with mapVar() function),
+ * that will modify the map upon assignment (note that you'd need MutableMap instead of read-only Map).
+ */
+class Female(val map: Map<String, Any?>) {
+  val name: String by Delegates.mapVal(map)
+  val age: Int by Delegates.mapVal(map)
+}
---
+++ investigate-kotlin/src/main/kotlin/com/freetmp/investigate/kotlin/syntax/BasicSyntax.kt
@@ -0,0 +1,111 @@
+package com.freetmp.investigate.kotlin.syntax
+
+/**
+ * Created by LiuPin on 2015/6/4.
+ */
+fun main(args: Array<String>) {
+  println("******************Conditional expression******************")
+  println(max(10, 20))
+  println(max(-1, -4))
+  println(max(3, 16))
+
+  println("******************Null checks******************")
+  println(parseInt("20"))
+  println(parseInt("20a"))
+
+  println("********************Is-checks and smart casts********************")
+  println(getStringLength("aaa"))
+  println(getStringLength(1))
+
+  println("*******************Use a while loop********************")
+  usingWhileLoop()
+
+  println("*******************Use a for loop********************")
+  usingForLoop()
+
+  println("********************Use ranges and in**********************")
+  usingRanges(4)
+
+  println("**********************Use when***********************")
+  useWhen()
+}
+
+fun max(a: Int, b: Int) = if (a > b) a else b
+
+// Return null if str does not hold a number
+fun parseInt(str: String): Int? {
+  try {
+    return Integer.parseInt(str)
+  } catch(e: Exception) {
+    println("One of the arguments isn't Int")
+  }
+  return null
+}
+
+fun getStringLength(obj: Any): Int? {
+  if(obj is String) return obj.length() // no cast to String is needed
+  return null
+}
+
+fun usingWhileLoop(){
+  var i = 0
+  while(i++ < 10){
+    print(i.toString() + " ")
+  }
+  println()
+}
+
+fun usingForLoop(){
+  for(i in 1..10){
+    print(i.toString() + " ")
+  }
+  println()
+}
+
+fun usingRanges(x: Int){
+  //Check if a number lies within a range:
+  var y = 10
+  if(x in 1..y-1) println("OK")
+
+  //Iterate over a range
+  for(a in 1..5) print("${a} ")
+  println()
+
+  //Check if a number is out of range:
+  var array = arrayListOf<String>()
+  array.add("aaa")
+  array.add("bbb")
+  array.add("ccc")
+
+  if(x !in 0..array.size() - 1)
+    println("Out: array has only ${array.size()} elements. x = ${x}")
+
+  //Check if a collection contains an object:
+  if("aaa" in array) println("Yes: array contains aaa")
+
+  if("ddd" in array) print("Yes: array contains ddd")
+  else println("No: array doesn't contains ddd")
+
+}
+
+class MyClass(){}
+
+fun useWhen(){
+  fun cases(obj: Any){
+    when (obj) {
+      1 -> println("One")
+      in 2..4 -> println("Between two and four")
+      "Hello" -> println("Greeting")
+      is Long -> println("Long")
+      !is String -> println("Not a string")
+      else -> println("Unknown")
+    }
+  }
+
+  cases("Hello")
+  cases(1)
+  cases(3)
+  cases(System.currentTimeMillis())
+  cases(MyClass())
+  cases("hello")
+}
