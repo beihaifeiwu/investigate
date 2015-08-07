adc05437f1d696f9207027252c756cce62254cab
liu pin <pin.liu@palmaplus.com>
2015-08-07 09:40:31 +0800

    jersey2 与 spring boot 整合

---
+++ investigate-spring-boot/src/main/java/com/freetmp/investigate/springboot/jersey2/JerseyConfig.java
@@ -0,0 +1,15 @@
+package com.freetmp.investigate.springboot.jersey2;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by LiuPin on 2015/8/6.
+ */
+@Component
+public class JerseyConfig extends ResourceConfig {
+  public JerseyConfig() {
+    register(GreetingOne.class);
+    register(GreetingTwo.class);
+  }
+}
