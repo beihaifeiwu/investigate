6b9a16329b56c4598bc6066353b56c35d1633e0f
beihaifeiwu <liuhuizhang@foxmail.com>
2016-04-03 15:09:50 +0800

    remove no use module

--- investigate-griffon/src/main/java/com/freetmp/investigate/griffon/Evaluator.java
+++
@@ -1,8 +0,0 @@
-package com.freetmp.investigate.griffon;
-
-/**
- * Created by LiuPin on 2016/3/17.
- */
-public interface Evaluator {
-  Object evaluate(String input);
-}
--- investigate-griffon/src/main/java/com/freetmp/investigate/griffon/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.freetmp.investigate.griffon;
-
-import griffon.swing.SwingGriffonApplication;
-
-public class Launcher {
-    public static void main(String[] args) throws Exception {
-        SwingGriffonApplication.run(SwingGriffonApplication.class, args);
-    }
-}
