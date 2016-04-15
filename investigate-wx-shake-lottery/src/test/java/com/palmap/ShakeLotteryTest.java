3d8e08f19921c08b04ed78d619307972408cb384
beihaifeiwu <liuhuizhang@foxmail.com>
2016-04-15 23:53:33 +0800

    investigate on spring

---
+++ investigate-wx-shake-lottery/src/test/java/com/palmap/ShakeLotteryTest.java
@@ -0,0 +1,12 @@
+package com.palmap;
+
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+/**
+ * Created by liupin on 2016/4/15.
+ */
+@SpringApplicationConfiguration(ShakeLotteryApplication.class)
+public class ShakeLotteryTest extends AbstractTransactionalJUnit4SpringContextTests {
+
+}
